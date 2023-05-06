4725ab50d6e3820969f4c28984a8665059f18239
Tre <therealltre@gmail.com>
2023-05-06 21:08:57 +0000

    master

---
+++ app/components/Footer.tsx
@@ -0,0 +1,138 @@
+import {
+  Box,
+  chakra,
+  Button,
+  Image,
+  Container,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  VisuallyHidden,
+  Input,
+  IconButton,
+  useColorModeValue
+} from "@chakra-ui/react";
+import { ReactNode } from "react";
+import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import { BiMailSend } from "react-icons/bi";
+
+const Logo = (props: any) => {
+  return (
+    <Button as={"a"} href="/" variant={"link"}>
+      <Image src="/Logo.png" alt="Logo" width="320px" height="fit" />
+    </Button>
+  );
+};
+
+const SocialButton = ({
+  children,
+  label,
+  href
+}: {
+  children: ReactNode;
+  label: string;
+  href: string;
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
+      w={8}
+      h={8}
+      cursor={"pointer"}
+      as={"a"}
+      href={href}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
+      _hover={{
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200")
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+const ListHeader = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+export default function Footer() {
+  return (
+    <Box
+      bg={useColorModeValue("#1E4939", "gray.900")}
+      color={useColorModeValue("white", "gray.200")}
+    >
+      <Container as={Stack} maxW={"6xl"} py={10} maxH={"fit-content"}>
+        <SimpleGrid
+          templateColumns={{ sm: "1fr 1fr", md: "2fr 1fr 1fr 2fr" }}
+          spacing={8}
+        >
+          <Stack spacing={6}>
+            <Box>
+              <Logo color={useColorModeValue("gray.700", "white")} />
+            </Box>
+            <Text fontSize={"sm"}>© 2023 Xact Score. All rights reserved</Text>
+            <Stack direction={"row"} spacing={6}>
+              <SocialButton label={"Twitter"} href={"#"}>
+                <FaTwitter />
+              </SocialButton>
+              <SocialButton label={"YouTube"} href={"#"}>
+                <FaYoutube />
+              </SocialButton>
+              <SocialButton label={"Instagram"} href={"#"}>
+                <FaInstagram />
+              </SocialButton>
+            </Stack>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Company</ListHeader>
+            <Link href={"#about"}>About us</Link>
+            <Link href={"#contact"}>Contact us</Link>
+            <Link href={"#features"}>Features</Link>
+            <Link href={"#testimonials"}>Testimonials</Link>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Support</ListHeader>
+            <Link href={"#"}>Help Center</Link>
+            <Link href={"#"}>Terms of Service</Link>
+            <Link href={"#"}>Legal</Link>
+            <Link href={"#"}>Privacy Policy</Link>
+            <Link href={"#faq"}>FAQ</Link>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Stay up to date</ListHeader>
+            <Stack direction={"row"}>
+              <Input
+                placeholder={"Your email address"}
+                bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+                border={1}
+                variant="outline"
+                _focus={{
+                  bg: "whiteAlpha.300"
+                }}
+              />
+              <IconButton
+                bg={useColorModeValue("white", "green.800")}
+                color={useColorModeValue("#1E4939", "white")}
+                _hover={{
+                  bg: "green.600"
+                }}
+                aria-label="Subscribe"
+                icon={<BiMailSend />}
+              />
+            </Stack>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
+}
